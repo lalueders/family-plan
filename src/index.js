cdfcc7bf304f4fefcaf1722e890c36f2e1e9df21
Lasse Lüders <lasselueders@gmx.de>
2022-04-10 17:03:39 +0200

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import GlobalStyles from './GlobalStyles.js';
@@ -7,8 +8,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <BrowserRouter>
+      <GlobalStyles />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
