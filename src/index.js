a5de23cec10fe148b169a044dac54c766cb63ce2
Lasse Lüders <lasselueders@gmx.de>
2022-04-10 17:07:19 +0200

    Configure App.js, Index.js, index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import GlobalStyles from './GlobalStyles.js';
 import reportWebVitals from './reportWebVitals';
